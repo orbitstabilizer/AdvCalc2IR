06fd09a740d5668908862e23ce65e8855b2307e7
orbitstabilizer <71184281+orbitstabilizer@users.noreply.github.com>
2023-04-20 08:30:34 +0300

    Close file, line number starts with 1, fix in prelog

--- src/main.c
+++ src/main.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdbool.h>
-#include <stdlib.h>
 #include <string.h> 
 #include "executor.h"
 #include "debug.h"
@@ -12,7 +11,7 @@ void prelog(FILE* fp);
 void epilog(FILE* fp);
 /*
 ; ModuleID = 'advcalc2ir'
-declare i32 @printf(i8*, ...)
+declare_var i32 @printf(i8*, ...)
 @print.str = constant [4 x i8] c"%d\0A\00"
 
 define i32 @lr(i32 %x, i32 %c) {
@@ -39,10 +38,9 @@ define i32 @not(i32 %x) {
 define i32 @main() {
 */
 void prelog(FILE* fp){
-  fprintf(fp, "; ModuleID = 'advcalc2ir'\n");
+  fprintf(fp, "; ModuleID = 'advcalc2ir'\n\n");
   fprintf(fp, "declare i32 @printf(i8*, ...)\n");
-  fprintf(fp, "@print.str = constant [4 x i8] c\"%%d\\0A\\00\"\n");
-  fprintf(fp, "define i32 @main() {\n");
+  fprintf(fp, "@print.str = constant [4 x i8] c\"%%d\\0A\\00\"\n\n");
   fprintf(fp, "define i32 @lr(i32 %%x, i32 %%c) {\n");
   fprintf(fp, "  %%l1 = shl i32 %%x, %%c \n");
   fprintf(fp, "  %%l2 = sub i32 32, %%c \n");
@@ -60,15 +58,17 @@ void prelog(FILE* fp){
   fprintf(fp, "define i32 @not(i32 %%x) {\n");
   fprintf(fp, "  %%n1 = xor i32 %%x, -1 \n");
   fprintf(fp, "  ret i32 %%n1 \n");
-  fprintf(fp, "}\n");
+  fprintf(fp, "}\n\n");
   fprintf(fp, "define i32 @main() {\n");
 
 }
 void epilog(FILE* fp){
-  fprintf(fp, "ret i32 0\n");
+  fprintf(fp, "  ret i32 0\n");
   fprintf(fp, "}\n");
 }
+
 int main(int argc, char* argv[]) {
+
   if (argc < 2) {
     LOG_ERROR("Usage: %s <filename>\n", argv[0]);
     return 1;
@@ -77,7 +77,7 @@ int main(int argc, char* argv[]) {
   if (fp == NULL) {
     LOG_ERROR("Could not open file %s", argv[1]);
   }
-  int line_number = 0 ; 
+  int line_number = 1 ;
   // split filename file.adv to file
   char *filename = strtok(argv[1], ".");
   char output_filename[MAX_FILENAME];
@@ -115,6 +115,7 @@ int main(int argc, char* argv[]) {
   if(dict)
     free_dict(dict);
   epilog(output_file);
+  fclose(fp);
   return 0;
 
 }
